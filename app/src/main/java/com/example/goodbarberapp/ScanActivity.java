6e172e35ecc164c56feb98aa493b7072038dea00
theoc2a <theocussac2a@gmail.com>
2020-12-17 17:28:31 +0100

    push de la 2eme vue (version scan)

--- app/src/main/java/com/example/goodbarberapp/ScanActivity.java
+++ app/src/main/java/com/example/goodbarberapp/ScanActivity.java
@@ -1,28 +1,26 @@
 package com.example.goodbarberapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+public class ScanActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scan);
 
-        Button ButtonConnexion = (Button) findViewById(R.id.btS);  //Appel du "button1"
+        Button ButtonConnexion = (Button) findViewById(R.id.btM);  //Appel du "button1"
         ButtonConnexion.setOnClickListener(new View.OnClickListener()      //Creation du listener sur ce bouton
         {
             public void onClick(View actuelView)    //au clic sur le bouton
             {
-                Intent intent = new Intent(MainActivity.this, ScanActivity.class);  //Lancer l'activité DisplayVue
+                Intent intent = new Intent(ScanActivity.this, MainActivity.class);  //Lancer l'activité DisplayVue
                 startActivity(intent);    //Afficher la vue
             }
         });
-
     }
 }
